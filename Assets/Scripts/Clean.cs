22c607685d2c7e8327799a903d9f058c8756a215
Ariadna Solis <91100060+Grimmoryx@users.noreply.github.com>
2022-05-08 18:05:58 -0500

    modificaciones

--- Assets/Scripts/Clean.cs
+++ Assets/Scripts/Clean.cs
@@ -2,39 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Clean : MonoBehaviour {
+public class Clean: MonoBehaviour {
 
 	[SerializeField]
-
 	private float destroyAfter = 2.0f;
 	public bool _bPartOfPool = false; 
 	private Vector3 _posInicial; 
 
-	private void Start() {
-		if (_bPartOfPool ) {
+	private void Start()
+	{
+		if (_bPartOfPool )
+		{
 			_posInicial = transform.position;
 			StartCoroutine(returnPositionOriginal(destroyAfter));
 		}
-		else {
+		else
+		{
 			Destroy(gameObject, destroyAfter);
 		}
 	}
 
 	public void DestroyObject(){
-		if (_bPartOfPool && destroyAfter > 0) {
+		if (_bPartOfPool && destroyAfter > 0)
+		{
 			gameObject.transform.position = _posInicial;
             gameObject.SetActive(false);
 		}
-		else {
+		else
+		{
 			Destroy(gameObject);
 		}
 	}
 
-	IEnumerator returnPositionOriginal(float pTimeWait) {
+	IEnumerator returnPositionOriginal(float pTimeWait)
+    {
         yield return new WaitForSeconds(pTimeWait);
         gameObject.transform.position = _posInicial;
 		gameObject.SetActive(false);
     }
 
 }
-
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,81 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody))]
 
 public class Spawner : MonoBehaviour {
-    
-	//Elementos (tipo) a spawnear
-	public GameObject[] obs;
-    /*
- * g-782490      
- */
-    [SerializeField]
-    private float timeObs = 10, //tiempor entre objetos a lanzar
-    timeIncrement = 0, // incremento de velocidad
-    spawnIncrement = 0,//incrementar el spawn 
-    minSpawn = 0.65f,//tiempo minimo de spawn
-    maxNum = 0;//numero maximo de objetos a lanzar
+    [SerializeField] 
+    private GameObject obstaculo;
+    public int obstt;
 
-    [SerializeField]
-    private int cacheObj = 0; //Cuantos objetos creamos previamente para tener cacheados
-    private int rdmElements = 0; //Elemento a spawnear random entre los del array. 
-	private int len; //Longitud del array de elementos
-	private float waitingTime = 1;
-	private float itemsSpawned = 0; //Elementos spawneados. 
-
-    private ArrayList gameObjSpawn;
-    private int lastSpawn = 0;
-
-	private void Awake() {
-        //Calculamos el tiempo de lanzamiento. 
-		waitingTime = waitingTime + Random.Range(0, timeIncrement); 
-        //Calculamos longitud del array de items que podemos lanzar
-        len = obs.Length;
-        if (cacheObj > 0){
-            GameObject temp;
-            gameObjSpawn = new ArrayList();
-            for (int i = 0; i< cacheObj; i++){
-                rdmElements = Random.Range(0, len);
-                temp = Instantiate(obs[rdmElements], transform.position,Quaternion.identity);
-                temp.SetActive(false);
-                gameObjSpawn.Add(temp);
-            }
+    public IEnumerator time() {
+        yield return new WaitForSeconds(2);
+        if(obstt>0) {
+            kawaii();
+            obstt-=1;
         }
-	}
+    }
 
-	void Update () {
-		//if (!GameManager._gm._paused) {
-            if (waitingTime <= 0 && (maxNum == 0 || maxNum > itemsSpawned)) {
-                lanza();
-                if (spawnIncrement >= 0 && timeObs > minSpawn) {
-                    timeObs -= spawnIncrement;
-                }
-				waitingTime = timeObs;
-                if (timeIncrement > 0f) {
-                    waitingTime += Random.Range(0, timeIncrement);
-                }
-                if (waitingTime < minSpawn) {
-                    waitingTime = minSpawn;
-                }
-				itemsSpawned += 1;
-			}
-			else {
-				waitingTime -= Time.deltaTime;
-			}
-		//}
-	}
-    
-	void lanza(){
-        if (cacheObj==0) {
-            rdmElements = Random.Range(0, len);			
-		    Instantiate(obs[rdmElements], transform.position, Quaternion.identity);
-        } else {
-            if (lastSpawn >= gameObjSpawn.Count) {
-                lastSpawn = 0;
-            }
-            GameObject halou = (UnityEngine.GameObject)gameObjSpawn[lastSpawn];
-            halou.SetActive(true);
-            lastSpawn += 1;
-        }
-	}
+    void Start() {
+    }
+
+    void Update() {
+        StartCoroutine(time());
+    }
+    void kawaii(){
+        float PosX;
+        float PosY;
+        float PosZ = -20f;
+        PosX = Random.Range(-9f, 9f); //Los limites del plano 
+        PosY = Random.Range(-9f, 9f);
+        var instance = Instantiate(obstaculo, new Vector3(PosX,PosY,PosZ),new Quaternion());
+        instance.GetComponent<Rigidbody>().AddForce(new Vector3(0,0,1),ForceMode.Impulse); 
+    }
+
+    void borrar() {}
 }
+    
